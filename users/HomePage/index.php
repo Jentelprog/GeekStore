4cecc68710d50e03ab13849d991ef4d7ae48365e
Mandiz44 <oceanusyo85@gmail.com>
2025-04-20 12:44:59 +0100

    Add:More CSS changes

--- users/HomePage/index.php
+++ users/HomePage/index.php
@@ -11,6 +11,7 @@ if (empty($_SESSION["user_id"])) {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>GeekStore</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -29,7 +30,9 @@ if (empty($_SESSION["user_id"])) {
 			<a href="#about">About</a>
 			<a href="#contact">Contact</a>
 			<form class="logout-form" action="logout.php" method="post">
-				<input type="submit" value="log out">
+			<button type="submit" name="logout" class="submit">
+			<i class="fa-solid fa-right-to-bracket"></i>
+			<button type="submit" name="logout" class="submit"></button>
 			</form>
 		</nav>
 	</header>
@@ -41,16 +44,16 @@ if (empty($_SESSION["user_id"])) {
 		</div>
 	</section>
 	<section id="shop">
-		<h2>Shop Anime & Manga Merch</h2>
+		<h2>Best Seller</h2>
 		<div class="product-container" id="product-list">
 			<div class="product-card">
 				<img src="../../img/sakamoto_toy.webp" alt="Anime Figure">
-				<h3>Anime Figure</h3>
+				<h3>Sakamoto Figure</h3>
 				<a href="../Products figures/index.html">Explore</a>
 			</div>
 			<div class="product-card">
 				<img src="../../img/sl_hoodie.webp" alt="Anime Hoodie">
-				<h3>Anime Hoodie</h3>
+				<h3>Solo Leveling Hoodie</h3>
 				<a href="../Products hoodie/index.html">Explore</a>
 			</div>
 			<div class="product-card">
@@ -58,8 +61,32 @@ if (empty($_SESSION["user_id"])) {
 				<h3>Anime stickers</h3>
 				<a href="../Products sticker/index.html">Explore</a>
 			</div>
+			<div class="product-card">
+				<img src="../../img/gfreddy_plush.jpg" alt="Anime stickers">
+				<h3>Golden Freddy Plushie</h3>
+				<a href="../Products sticker/index.html">Explore</a>
+			</div>
+			</div>
 		</div>
 	</section>
+	<section id="pose">
+  <div class="poseimg">
+    <img src="../../img/pose1.jpg" alt="Pose1">
+    <div class="overlay"><a class="overlay-link" href="../Products figures/index.html">purchase jujutsu kaisen</a></div>
+	
+  </div>
+
+  <div class="poseimg">
+    <img src="../../img/pose2.jpg" alt="Pose2">
+    <div class="overlay"><a class="overlay-link" href="../Products figures/index.html">purchase blue lock</a></div>
+	
+  </div>
+
+  <div class="poseimg">
+    <img src="../../img/pose3.jpg" alt="Pose3">
+    <div class="overlay"><a class="overlay-link" href="../Products figures/index.html">purchase kaiju n°8</a></div>
+  </div>
+</section>
 	<section id="contact">
 		<h2>Contact Us</h2>
 		<p>Email: support@geekstore.com</p>
@@ -68,7 +95,7 @@ if (empty($_SESSION["user_id"])) {
 
 	<footer id="about">
 		<h2>About GeekStore</h2>
-		<p>We’re passionate about bringing anime and manga fans the best merch in the galaxy.</p>
+		<p>We're passionate about bringing anime and manga fans the best merch in the galaxy.</p>
 	</footer>
 
 </body>
